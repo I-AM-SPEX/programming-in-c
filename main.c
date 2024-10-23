6c844a0da46f8f914f0504905c4501aeb61188b9
samuel williams <samuelspexy@gmail.com>
2024-10-23 15:27:42 +0200

    isArmstrongNumber function

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+
+bool isArmstrongNumber(int number)
+{
+    int sum = 0;
+    int numberOfDigits = floor(log10(abs(number))) + 1;
+    int num = number;
+    while (num > 0)
+    {
+        int mod = num % 10;
+        sum = sum + pow(mod, numberOfDigits);
+        num /= 10;
+    }
+    printf("%d \n",sum);
+    printf("%d \n",number);
+    return (number == sum) ? true : false;
+}
+
+int main()
+{
+    int input;
+    printf("Check Armstrong Number\n");
+    printf("Enter Interger: \n");
+    scanf("%d",&input);
+    bool result = isArmstrongNumber(input);
+    printf("Armstrong Number: %s \n", result ? "true" : "false");
+    return 0;
+}
