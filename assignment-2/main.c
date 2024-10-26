151ba2c2e7f6bd60c3a5b86bcd43e201bfd5bebf
samuel williams <samuelspexy@gmail.com>
2024-10-26 22:22:45 +0200

    larger sum program

--- assignment-2/main.c
+++ assignment-2/main.c
@@ -2,38 +2,63 @@
 #include <stdio.h>
 #include <math.h>
 
+int main()
+{
+    // Q1
+    // int number;
+    // int n;
+    // int sum = 0;
+    // int digitCount = 0;
+    // printf("AMSTRONG NUMBER CHECKER\n");
+    // printf("Enter number: \n");
+    // scanf("%d",&number);
+    // n = number;
+    // while (n > 0)
+    // {
+    //     n/=10;
+    //     digitCount++;
+    // }
+    // n = number;
 
-int main () {
-    int number;
-    int n;
-    int sum = 0;
-    int digitCount = 0;
-    printf("AMSTRONG NUMBER CHECKER\n");
-    printf("Enter number: \n");
-    scanf("%d",&number);
-    n = number;
-    while (n > 0)
+    // while (n > 0)
+    // {
+    //     int digit = n % 10;
+    //     sum+=pow(digit,digitCount);
+    //     n/=10;
+    // }
+
+    // if(sum == number) {
+    //     printf("%d is an amstrong number\n",number);
+    // }else {
+    //     printf("%d is not an armstrong number\n",number);
+    // }
+
+    // Q2
+
+    int oddSum = 0;
+    int evenSum = 0;
+    int arrLength;
+    printf("Larger Sum Program:\n");
+    printf("Input length of array:\n");
+    scanf("%d", &arrLength);
+    int arr[arrLength];
+    for (int i = 0; i < arrLength; i++)
     {
-        n/=10;
-        digitCount++;
+        printf("Enter element at index %d\n", i);
+        scanf("%d", &arr[i]);
     }
-    n = number;
 
-    while (n > 0)
+    for (int j = 0; j < arrLength; j++)
     {
-        int digit = n % 10;
-        sum+=pow(digit,digitCount);
-        n/=10;
+        if(arr[j] % 2 == 0)   {
+            evenSum+=arr[j];
+        }else {
+            oddSum+=arr[j];
+        }
     }
 
-    if(sum == number) {
-        printf("%d is an amstrong number\n",number);
-    }else {
-        printf("%d is not an armstrong number\n",number);
-    }
-    
+
     
 
-
-
+    (evenSum > oddSum)? printf("Larger sum is %d\n",evenSum) : printf("Larger sum is %d\n",oddSum);
 }
