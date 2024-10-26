4af7b0dec281087cf077ecd102c233c8486713d6
samuel williams <samuelspexy@gmail.com>
2024-10-26 23:01:08 +0200

    item calculator

--- assignment-2/main.c
+++ assignment-2/main.c
@@ -2,9 +2,55 @@
 #include <stdio.h>
 #include <math.h>
 
+int calculateTotalPrice(int prices[], int quantity[], int arrLength)
+{
+    int total = 0;
+    for (int i = 0; i < arrLength; i++)
+    {
+        total += prices[i] * quantity[i];
+    }
+
+    return total;
+}
+
+
+int getDiscount(int amount) {
+     int discount = 0;
+    if (amount < 1000)
+    {
+        discount = 5;
+    }
+    else if (amount > 1000 && amount < 5000)
+    {
+        discount = 10;
+    }
+    else if (amount > 5000)
+    {
+        discount = 15;
+    }
+    return discount;
+}
+
+
+void outputItemPrices(int discount,int total,int prices[],int quantity[],int arrLength)
+{
+    int discounted = (discount / 100.00) * total;
+
+    printf("Item        Price         Quantity      Subtotal\n");
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        printf("Item %-5d   BWP %-10.2d %-10d BWP %-10.2d\n", i + 1, prices[i], quantity[i], prices[i] * quantity[i]);
+    }
+
+    printf("TOTAL                           BWP%5.2d\n", total);
+    printf("Discount %3d%%                   BWP%5.2d\n", discount, discounted);
+    printf("GRAND TOTAL                     BWP%5.2d\n", total - discounted);
+}
+
 int main()
 {
-    // Q1
+    // Q1LauNDARRY
     // int number;
     // int n;
     // int sum = 0;
@@ -35,30 +81,47 @@ int main()
 
     // Q2
 
-    int oddSum = 0;
-    int evenSum = 0;
+    // int oddSum = 0;
+    // int evenSum = 0;
+    // int arrLength;
+    // printf("Larger Sum Program:\n");
+    // printf("Input length of array:\n");
+    // scanf("%d", &arrLength);
+    // int arr[arrLength];
+    // for (int i = 0; i < arrLength; i++)
+    // {
+    //     printf("Enter element at index %d\n", i);
+    //     scanf("%d", &arr[i]);
+    // }
+
+    // for (int j = 0; j < arrLength; j++)
+    // {
+    //     if(arr[j] % 2 == 0)   {
+    //         evenSum+=arr[j];
+    //     }else {
+    //         oddSum+=arr[j];
+    //     }
+    // }
+
+    // (evenSum > oddSum)? printf("Larger sum is %d\n",evenSum) : printf("Larger sum is %d\n",oddSum);
+
+    // Q3
     int arrLength;
-    printf("Larger Sum Program:\n");
-    printf("Input length of array:\n");
+    printf("Item Prices Calculator: \n");
+    printf("Enter number of items: \n");
     scanf("%d", &arrLength);
-    int arr[arrLength];
+    int prices[arrLength];
+    int quantity[arrLength];
+
     for (int i = 0; i < arrLength; i++)
     {
-        printf("Enter element at index %d\n", i);
-        scanf("%d", &arr[i]);
+        printf("Enter price of item at index %d\n", i);
+        scanf("%d", &prices[i]);
+        printf("Enter item quantity at index %d\n", i);
+        scanf("%d", &quantity[i]);
     }
 
-    for (int j = 0; j < arrLength; j++)
-    {
-        if(arr[j] % 2 == 0)   {
-            evenSum+=arr[j];
-        }else {
-            oddSum+=arr[j];
-        }
-    }
-
-
-    
-
-    (evenSum > oddSum)? printf("Larger sum is %d\n",evenSum) : printf("Larger sum is %d\n",oddSum);
+    int totalSum = calculateTotalPrice(prices,quantity,arrLength);
+    int discount  = getDiscount(totalSum);
+    outputItemPrices(discount,totalSum,prices,quantity,arrLength);
 }
